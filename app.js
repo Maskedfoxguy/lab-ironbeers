dd1e9b211f3f906c7443ef17c16d2fb74f76cce6
Adam Epinger <76474223+Maskedfoxguy@users.noreply.github.com>
2023-12-01 23:35:52 +0100

    change

--- app.js
+++ app.js
@@ -14,12 +14,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers' , (req, res) => {
+
+  .getBeers()
+  .then(beersFromApi => res.render('beers' , { beers: beersFromApi}))
+
+  .catch(error => console.log(error));
+
+});
+
+app.get('/random-beer' , (req, res) =>{
+
+  .getRandom()
+  .then(responseFromApi => {
+
+    const randomBeer = responseFromApi[0];
+    res.render('random-beer' , {beer: randomBeer});
+  })
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
